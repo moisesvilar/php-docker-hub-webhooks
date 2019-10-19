96acb2704b7df5fa7a55e8f5021b65f2702f4df9
moisesvilar <moisvv@gmail.com>
2019-10-19 18:51:01 +0200

    Use  instead of

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
   $trigger_tag = "master";
-  $post_data = json_decode($HTTP_RAW_POST_DATA, true);
+  $post_data = json_decode($_POST, true);
   $tag = $post_data["push_data"]["tag"];
   if ($tag === $trigger_tag) {
     $image = "user/repo:" . $trigger_tag;
