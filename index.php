a84944e2acc7414e9be38486ada5a688a55c8a0b
moisesvilar <moisvv@gmail.com>
2019-10-19 19:06:21 +0200

    Use file_get_contents instead of $_POST

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-  $trigger_tag = "master";
-  $post_data = json_decode($_POST, true);
+  $trigger_tag = "latest";
+  $post_data = json_decode(file_get_contents('php://input'), true);
   $tag = $post_data["push_data"]["tag"];
   if ($tag === $trigger_tag) {
     $image = "user/repo:" . $trigger_tag;
